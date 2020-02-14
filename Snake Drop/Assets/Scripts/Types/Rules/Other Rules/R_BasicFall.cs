a24f91eeef3fd81b532e66cf6ce2d1755c7ffe8f
Fano <n.j.dobek@gmail.com>
2020-02-13 19:24:19 -0700

    uhjhhhh

--- Snake Drop/Assets/Scripts/Types/Rules/Other Rules/R_BasicFall.cs
+++ Snake Drop/Assets/Scripts/Types/Rules/Other Rules/R_BasicFall.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Rules/General/Fall Rules/Basic Fall")]
 public class R_BasicFall : Rule
 {
-    public MoveRule BasicMoveRule;
+    public MoveRule MoveRule;
 
     protected override void Action(Block block, PlayerManager player = null)
     {
         BlockSlot destination = block.Neighbor(GameManager.Direction.DOWN);
-        while (BasicMoveRule.CanMoveTo(block, destination, player) && destination.playGrid == block.Slot.playGrid)
+        while (MoveRule.CanMoveTo(block, destination, player) && destination.playGrid == block.Slot.playGrid)
         {
-            BasicMoveRule.OnMove(block, destination, player);
+            MoveRule.OnMove(block, destination, player);
             destination = block.Neighbor(GameManager.Direction.DOWN);
         }
     }
