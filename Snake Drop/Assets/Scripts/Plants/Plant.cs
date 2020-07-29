cf06f1747bbeaea30ebce00a5de64da6e01c07c1
Fano <n.j.dobek@gmail.com>
2020-07-29 12:03:08 -0600

    Screwed up plants

--- Snake Drop/Assets/Scripts/Plants/Plant.cs
+++ Snake Drop/Assets/Scripts/Plants/Plant.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plant : MonoBehaviour
+public class Plant : MonoBehaviour, IComparable<Plant>
 {
     public IGrowable growable;
 
     [HideInInspector]
     public int xp = 0;
     public int xpPerGrow = 5;
+
+    public bool CurrentlyGrowing;
+
+    public void AddXP(int amount)
+    {
+        if (CurrentlyGrowing) xp += amount;
+    }
 
     private void Awake()
     {
@@ -28,5 +36,17 @@ public class Plant : MonoBehaviour
     {
         xp = 0;
         growable.Grow();
+    }
+
+    public int CompareTo(Plant plant)
+    {
+        return ComparePlants(this, plant);
+    }
+
+    public static int ComparePlants(Plant plant1, Plant plant2)
+    {
+        if (plant1.growable.GrowthStage < plant2.growable.GrowthStage) return -1;
+        if (plant1.growable.GrowthStage > plant2.growable.GrowthStage) return 1;
+        return 0;
     }
 }
