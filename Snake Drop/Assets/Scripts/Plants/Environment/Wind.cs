12e20213f4617d9008bc62ce3a131b6b507b724d
Fano <n.j.dobek@gmail.com>
2020-11-17 15:51:27 -0700

    Revert "Revert "smol changes""
    This reverts commit d1741471ac3af0c991b11eed100b95d9e8d88827.

--- Snake Drop/Assets/Scripts/Plants/Environment/Wind.cs
+++ Snake Drop/Assets/Scripts/Plants/Environment/Wind.cs
@@ -7,14 +7,16 @@ public class Wind : ScriptableObject
 {
     public float xDistanceModifier;
     public float yDistanceModifier;
+    public AnimationCurve windCurve;
     public float periodLength;
+    public float scalar;
 
     public float GetWindSpeed(Transform location)
     {
         //result from -1 to 1
         float result = Mathf.Sin((Time.time + (xDistanceModifier * location.position.x) + (yDistanceModifier * location.position.y)) / periodLength);
         //result from 0 to 1
-        return (result + 1f) / 2;
+        return windCurve.Evaluate((result + 1f) / 2) * scalar;
     }
 
 }
