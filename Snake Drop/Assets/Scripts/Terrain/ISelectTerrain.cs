9b761729084d3adfd82832996a0c59dbf65ca0c9
LyssaJaeger <alyssa.dobek@gmail.com>
2021-01-19 13:12:21 -0700

    Added Terrain

---
+++ Snake Drop/Assets/Scripts/Terrain/ISelectTerrain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISelectTerrain
+{
+    void SelectTerrain(GameObject selection, ISpawnTerrain terrainSpawner);
+    GameObject SelectedTerrain 
+    {
+        get;
+    }
+}
---
+++ Snake Drop/Assets/Scripts/Terrain/TerrainSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSpawner : MonoBehaviour, ISpawnTerrain
+{
+ 
+    public ISelectTerrain terrainSelector;
+    private GameObject terrain;
+
+    private void Start()
+    {
+        terrainSelector = GetComponentInParent<ISelectTerrain>();
+    }
+    public void SpawnTerrain()
+    {
+        terrain = Instantiate(terrainSelector.SelectedTerrain, transform);
+    }
+    public void DestroyTerrain()
+    {
+        if (terrain)
+        {
+            Destroy(terrain);
+        }
+    }
+}
