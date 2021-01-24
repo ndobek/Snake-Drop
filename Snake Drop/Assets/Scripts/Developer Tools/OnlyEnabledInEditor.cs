cd264e069461c2601887e89b3ee85172055dbcb1
LyssaJaeger <alyssa.dobek@gmail.com>
2021-01-24 11:52:14 -0700

    cleaning stuff up, starting to develop a weather system no judge

---
+++ Snake Drop/Assets/Scripts/Developer Tools/OnlyEnabledInEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyEnabledInEditor : MonoBehaviour
+{
+    //TODO: this is borken make it work 
+    void Start()
+    {
+        Destroy(this);
+    }
+
+}
---
+++ Snake Drop/Assets/Scripts/Planet/Visual Effects/Animated Lighting/Animated Light.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedLight : MonoBehaviour, IAnimatableLight
+{
+
+    private List<LightPreset> presets;
+    public List<LightPreset> Presets
+    { get { return presets; } }
+  
+}
