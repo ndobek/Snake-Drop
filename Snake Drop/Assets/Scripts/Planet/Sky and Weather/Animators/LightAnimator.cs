dac334edec39cf29f30011ef5a6e8549c8821da6
LyssaJaeger <alyssa.dobek@gmail.com>
2021-01-26 18:08:51 -0700

    changed shadowcasters

--- Snake Drop/Assets/Scripts/Planet/Sky and Weather/Animators/LightAnimator.cs
+++ Snake Drop/Assets/Scripts/Planet/Sky and Weather/Animators/LightAnimator.cs
@@ -7,17 +7,20 @@ public class LightAnimator : MonoBehaviour, IEffectAnimator
 {
     [SerializeField]
     private List<IAnimatableEffect> effects;
-    public List<IAnimatableEffect> Effects { get; }
+    public List<IAnimatableEffect> Effects { get { return effects; } }
+   
+    //This class shouldn't have the presetData, because how would it know which ones go with which light? 
+    //instead, the light interface needs to have different methods you can call on it, like cool() but that sucks,
+    //because you would have to add them to a billion things...it's better if you just add them here. that means you need
+    //to also be able to set the light type, the cookie, the order, etc so that it can just cycle through a bunch of lights and
+    //make them whatever they need to be.
+    //[SerializeField]
+    //private List<IPresetData> presetData;
+    //public List<IPresetData> PresetData { get { return presetData; } }
 
-    [SerializeField]
-    private List<IPresetData> presetData;
-    public List<IPresetData> PresetData { get { return presetData; } }
 
 
-    public void Start()
-    {
-
-    }
+   
     public void UpdateAnimations()
     {
     }
