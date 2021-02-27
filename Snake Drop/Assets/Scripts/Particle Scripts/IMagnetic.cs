8e8544be60136af4b6eac0805267dfaf580d2292
LyssaJaeger <alyssa.dobek@gmail.com>
2021-02-26 18:58:33 -0700

    Found Particle System Force Field
    ...I'll add it now...

---
+++ Snake Drop/Assets/Scripts/Particle Scripts/IMagnetic.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMagnetic
+{
+    void Magnetize();
+    bool InRange {get; set;}
+}
---
+++ Snake Drop/Assets/Scripts/Particle Scripts/MagneticParticles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticParticles : MonoBehaviour, IMagnetic
+{
+    private bool inRange;
+    public bool InRange
+    {
+        get { return inRange; }
+        set { inRange = value; }
+    }
+    public void Magnetize()
+    {
+
+    }
+}
