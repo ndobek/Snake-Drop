1b933a98d92d3d62754885b7bdbba215f6e4d904
LyssaJaeger <alyssa.dobek@gmail.com>
2021-03-04 15:31:27 -0700

    THANK GOODNESS IT WORKS

--- Snake Drop/Assets/Scripts/Particles and Effects/Sky and Weather/Animated Effects/VolumeAnimator.cs
+++ Snake Drop/Assets/Scripts/Particles and Effects/Sky and Weather/Animated Effects/VolumeAnimator.cs
@@ -4,47 +4,26 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class VolumeAnimator : MonoBehaviour, IEffectAnimator<ColorCurves, VolumeState, VolumePreset>
+public class VolumeAnimator : MonoBehaviour, IEffectAnimator<Transform, VolumeProfile, VolumePreset>
 {
 
-    public TextureCurve hueVsSat;
-    public TextureCurveParameter hueVsSatParameter;
-    public Keyframe key;
-    public Volume volume;
-    public ColorCurves colorCurves;
-    public VolumeProfile profile;
-    private float time;
     [SerializeField]
     private VolumePreset initialState;
     public VolumePreset InitialState { get => initialState; set { initialState = value; } }
 
-    private VolumeState currentState;
-    public VolumeState CurrentState { get => currentState; set {currentState = value; } }
+    private VolumeProfile currentState;
+    public VolumeProfile CurrentState { get => currentState; set {currentState = value; } }
 
-    public void InitializeCurves()
-    {
-        currentState = initialState.volumeState;
-        profile.TryGet<ColorCurves>(out colorCurves);
-        hueVsSatParameter = new TextureCurveParameter(initialState.volumeState.hueVsSat, true);
-        colorCurves.hueVsSat = hueVsSatParameter;
-        
-        volume.profile = profile;
-        
-    }
+    
     private void Start()
     {
-        InitializeCurves();
     }
-    public void Animate(VolumeState keyframe1, VolumeState keyframe2, float t)
+    public void Animate(VolumeProfile keyframe1, VolumeProfile keyframe2, float t)
     {
-        CurrentState.hueVsSat = keyframe1.hueVsSat;
-        UpdateEffect(colorCurves, keyframe2);
-        time = t;
+    
     }
 
-    public void UpdateEffect(ColorCurves effect, VolumeState nextFrame)
+    public void UpdateEffect(Transform effect, VolumeProfile nextFrame)
     {
-        effect.hueVsSat.Interp(CurrentState.hueVsSat, nextFrame.hueVsSat, time);
-        volume.profile = profile;
-    }
+    } 
 }
--- Snake Drop/Assets/Scripts/Particles and Effects/Sky and Weather/Presets/Post Processing Presets/VolumePreset.cs
+++ Snake Drop/Assets/Scripts/Particles and Effects/Sky and Weather/Presets/Post Processing Presets/VolumePreset.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 [CreateAssetMenu(menuName = "Effect Presets/Volume")]
 public class VolumePreset : ScriptableObject
 {
-    public VolumeState volumeState;
- 
+    public VolumeProfile volumeState;
 }
