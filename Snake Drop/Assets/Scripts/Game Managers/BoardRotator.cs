202ff1e75adcc112a8f1e50717d60268607055d2
LyssaJaeger <alyssa.dobek@gmail.com>
2021-03-28 14:09:27 -0600

    Fixed the new planet spinning animation

--- Snake Drop/Assets/Scripts/Game Managers/BoardRotator.cs
+++ Snake Drop/Assets/Scripts/Game Managers/BoardRotator.cs
@@ -5,17 +5,51 @@ using UnityEngine;
 public class BoardRotator : MonoBehaviour
 {
     [SerializeField]
-    private float rotationLerpSpeed = 1;
-    private float targetRotation = 0;
+    public float targetRotation = 0;
     public int EnterSlotMoveDistance = 11;
     public bool KeepEntranceSlotOnOneSide;
     public Directions.Direction EntranceSide;
 
+    //for board spin animations
+    public AnimationCurve rotationCurve; 
+    public float rotationTime; 
+    public float ClampedTime 
+    { 
+        get 
+        { 
+            return Mathf.Clamp(rotationTime, 0, rotationDuration); 
+        } 
+    }
+    //TODO: make some of these private when testing is done
+    public float rotationDuration;
+    public bool rotating = false;
+    public float savedTargetRotation = 0f;
+    public float totalRotationDegrees = 0f;
+    public bool targetAquired = false;
+    public float rotationThreshold;
+    public float currentStartingPoint;
+    public bool RotationDirection 
+    { 
+        get 
+        { 
+            return (totalRotationDegrees > 0f);
+        } 
+    }
+    //for particle reactions
+    public float reactionThreshold;
+    public bool reacted = false;
+    public List<IReact> reactToSpin = new List<IReact>();
+
+    
+   
     [HideInInspector]
     public Directions.Direction currentDirection = Directions.Direction.UP;
-
     private bool EntranceMismatched = false;
-
+    private void Start()
+    {
+        rotationTime = rotationDuration;
+        
+    }
     public void Update()
     {
         if (!GameManager.instance.playerManagers[0].RoundInProgress && KeepEntranceSlotOnOneSide)
@@ -25,18 +59,140 @@ public class BoardRotator : MonoBehaviour
                 EntranceMismatched = false;
                 RotateEntranceToMatchBoard();
             }
-            RotateBoardToMatchEntrance();
             
+            RotateBoardToMatchEntrance();
         }
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, targetRotation), rotationLerpSpeed);
+        rotationTime += Time.deltaTime;
+        if (!Mathf.Approximately(transform.rotation.eulerAngles.z, targetRotation)  && rotating == false)
+        {
+            NewTarget();
+        }
+        if (rotating == true)
+        {
+            if(targetRotation != savedTargetRotation)
+            {
+                
+                NewTarget(FindStartingPoint(), true);
+            }
+            
+            AnimateRotation();
+        }
+        
+        
     }
-    
-    public void SetRotation(Directions.Direction UpDirection)
+    #region Alyssa Methods
+    private float FindStartingPoint()
     {
-        currentDirection = UpDirection;
-        OnRotate();
+
+        float rotationDegrees = transform.rotation.eulerAngles.z;
+        if (RotationDirection)
+        {
+
+            return (Mathf.Ceil(rotationDegrees / 90f))*90f;
+        }
+      
+        else
+        {
+            return (Mathf.Floor(rotationDegrees / 90f)) * 90f;
+        }
+    }
+    private float FindNearestTarget(float rotationDegrees)
+    {
+        float quadrant = rotationDegrees / 90f;
+        if (Mathf.Approximately(quadrant, 0f))
+        {
+            return rotationDegrees;
+        }
+       
+        if (Mathf.Abs(rotationDegrees % 90) > .5f)
+        {
+            return Mathf.Ceil(quadrant) * 90f;
+        }
+        else
+        {
+            return Mathf.Floor(quadrant) * 90f;
+        }
+        
+        
     }
 
+    private void SetNewTrajectory(float newStartingPoint)
+    {
+        
+        totalRotationDegrees = Mathf.DeltaAngle(newStartingPoint, targetRotation);
+
+    }
+    private void NewTarget()
+    {
+        NewTarget(FindNearestTarget(transform.rotation.eulerAngles.z), false);
+        
+    }
+    private void NewTarget(float startingPoint, bool reverseMidRotation)
+    {
+
+        if (reverseMidRotation == true)
+        {
+            rotationTime = rotationDuration - rotationTime;
+        }
+        else
+        {
+            rotationTime = 0;
+        }
+
+
+        SetNewTrajectory(startingPoint);
+        currentStartingPoint = startingPoint;
+        savedTargetRotation = targetRotation; 
+
+        rotating = true;
+        targetAquired = true;
+
+    }
+    private float RotationPercent()
+    {
+     
+        return ClampedTime / rotationDuration;
+    }
+
+ 
+    
+    private void AnimateRotation()
+    {
+        if (targetAquired == true)
+        {
+
+            if (reacted == false && RotationPercent() >= reactionThreshold)
+            {
+                foreach (IReact react in reactToSpin)
+                {
+                    react.React();
+                }
+                reacted = true;
+            }
+        
+            float rotationStep = rotationCurve.Evaluate(RotationPercent()) * 90f;
+            transform.rotation = Quaternion.RotateTowards(Quaternion.Euler(0, 0, currentStartingPoint), Quaternion.Euler(0, 0, targetRotation), rotationStep);
+
+        }
+        if (Mathf.Approximately(transform.rotation.eulerAngles.z, targetRotation))
+        {
+
+            rotating = false;
+            reacted = false;
+            targetAquired = false;
+            
+        }
+       
+    }
+#endregion
+
+
+
+    public void SetRotation(Directions.Direction UpDirection)
+    {
+        currentDirection = UpDirection;      
+        OnRotate();
+    }
     public void RotateClockwise()
     {
         DoRotation(true);
@@ -45,22 +201,18 @@ public class BoardRotator : MonoBehaviour
     {
         DoRotation(false);
     }
-
     private void DoRotation(bool clockwise)
     {
-
         bool roundInProgress = GameManager.instance.playerManagers[0].RoundInProgress;
         Directions.Direction newDirection = clockwise ? Directions.GetClockwiseNeighborDirection(currentDirection) : Directions.GetCounterClockwiseNeighborDirection(currentDirection);
-
+        
         if (!(KeepEntranceSlotOnOneSide && !roundInProgress)) SetRotation(newDirection);
         if (KeepEntranceSlotOnOneSide)
         {
             if (roundInProgress) EntranceMismatched = true;
             else RotateEntranceSlot(clockwise);
         }
-
     }
-
     public void RotateEntranceSlot(bool clockwise)
     {
         int i = 0;
@@ -70,7 +222,6 @@ public class BoardRotator : MonoBehaviour
             GameManager.instance.playerManagers[0].MoveWaitSlot(clockwise);
         }
     }
-
     public void RotateEntranceSlotTo(Directions.Direction side)
     {
         Directions.Direction currentSide = GameManager.instance.playerManagers[0].enterSlot.GetEdgeInfo().direction();
@@ -83,7 +234,6 @@ public class BoardRotator : MonoBehaviour
             RotateEntranceSlot(true);
         }
     }
-
     public void RotateEntranceToMatchBoard()
     {
         RotateEntranceSlotTo(currentDirection);
@@ -92,14 +242,11 @@ public class BoardRotator : MonoBehaviour
     {
         SetRotation(Directions.TranslateDirection(GameManager.instance.playerManagers[0].enterSlot.GetEdgeInfo().direction(), EntranceSide));
     }
-
     private void OnRotate()
     {
         UpdateRotation();
         GameManager.instance.playerManagers[0].playGrid.InvokeGridAction();
     }
-
-
     public void UpdateRotation()
     {
         switch (currentDirection)
---
+++ Snake Drop/Assets/Scripts/Particles and Effects/Particle Scripts/ParticleReactToSpin.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ParticleReactToSpin : MonoBehaviour, IReact
+{
+    private ParticleSystem pSystem;
+    private List<ParticleSystem.Particle> newParticlesBehindPlanet = new List<ParticleSystem.Particle>();
+    private List<ParticleSystem.Particle> particlesLeavingPlanet = new List<ParticleSystem.Particle>();
+    private List<ParticleSystem.Particle> totalParticlesBehindPlanet = new List<ParticleSystem.Particle>();
+    
+    private Rigidbody rigidBody;
+    public BoardRotator boardRotator;
+    public float radialVMultiplier;
+    public float rotationalVMultiplier;
+    private void Start()
+    {
+        pSystem = GetComponent<ParticleSystem>();
+        rigidBody = GetComponent<Rigidbody>();
+        boardRotator.reactToSpin.Add(this);
+    }
+    private void OnParticleTrigger()
+    {
+        totalParticlesBehindPlanet.Clear();
+        pSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Inside, totalParticlesBehindPlanet);
+        //pSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, newParticlesBehindPlanet);
+        //if (newParticlesBehindPlanet.Count > 0)
+        //{
+        //    for (int i = 0; i < newParticlesBehindPlanet.Count; i++)
+        //    {
+                
+        //        totalParticlesBehindPlanet.Add(newParticlesBehindPlanet[i]);
+        //    }
+            
+        //}
+        //pSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, particlesLeavingPlanet);
+        //if (particlesLeavingPlanet.Count > 0)
+        //{
+        //    for(int i = 0; i < particlesLeavingPlanet.Count; i++)
+        //    {
+                
+        //        totalParticlesBehindPlanet.Remove(particlesLeavingPlanet[i]);
+        //    }
+        //}
+
+
+
+    }
+    public void React()
+    {
+        #region bad
+        //for (int i = 0; i < totalParticlesBehindPlanet.Count; i++)
+        //{
+        //    ParticleSystem.Particle particle = totalParticlesBehindPlanet[i];
+        //    Vector3 radialV = (particle.position - boardRotator.transform.position).normalized * radialVMultiplier;
+
+        //    Vector3 rotationalV = (rigidBody.GetPointVelocity(particle.position) * rotationalVMultiplier) ;
+        //    particle.velocity = rotationalV + radialV;
+        //    totalParticlesBehindPlanet[i] = particle;
+        //}
+        //pSystem.SetParticles(totalParticlesBehindPlanet.ToArray());
+        ////don't cursed code uncomment inside
+        ///
+        #endregion
+        #region ehh
+        //int count = pSystem.particleCount;
+        //ParticleSystem.Particle[] p = new ParticleSystem.Particle[count];
+        //pSystem.GetParticles(p);
+        //for (int i = 0; i < totalParticlesBehindPlanet.Count; i++)
+        //{
+
+        //    int pIndex = Array.IndexOf(p, totalParticlesBehindPlanet[i]);
+        //    if(pIndex >= 0)
+        //    {
+
+        //        Vector3 radialV = (p[pIndex].position - boardRotator.transform.position).normalized * radialVMultiplier;
+        //        p[pIndex].velocity = (rigidBody.GetPointVelocity(p[pIndex].position) * rotationalVMultiplier + radialV);
+
+        //    }
+        //}
+        #endregion
+        int count = pSystem.particleCount;
+        ParticleSystem.Particle[] p = new ParticleSystem.Particle[count];
+        pSystem.GetParticles(p);
+        for (int i = 0; i < count; i++)
+        {
+            if (totalParticlesBehindPlanet.Contains(p[i]))
+            {
+                Vector3 radialV = (p[i].position - boardRotator.transform.position).normalized * radialVMultiplier;
+                p[i].velocity = (rigidBody.GetPointVelocity(p[i].position) * rotationalVMultiplier) + radialV;
+            }
+        }
+        //this one works but is slow
+        pSystem.SetParticles(p);
+    }
+
+
+
+}
