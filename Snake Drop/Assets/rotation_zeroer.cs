6667d255850d9f9823cfcd5a1202f61a4b994a74
Fano <n.j.dobek@gmail.com>
2022-01-21 09:52:27 -0700

    Made waitslots move but not very well

--- Snake Drop/Assets/rotation_zeroer.cs
+++ Snake Drop/Assets/rotation_zeroer.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class rotation_zeroer : MonoBehaviour
 {
     private BoardRotator boardRotator;
-    private void Awake() {
+    private BlockSlot slot;
+    private void Start() {
         boardRotator = GameManager.instance.boardRotator;
+        slot = GetComponent<BlockSlot>();
     }
     void Update()
     {
-        transform.rotation = Quaternion.Euler(new Vector3(0,0,boardRotator.targetRotation));
+        if (slot.Block.blockType != GameManager.instance.GameModeManager.GameMode.TypeBank.snakeHeadType)
+        {
+            transform.rotation = boardRotator.transform.rotation;
+        }
+        else
+        {
+            transform.localRotation = Quaternion.identity;
+        }
+
     }
 }
