db0e07f12a4f3cde9c01810c118715931c43d9ac
Fano <n.j.dobek@gmail.com>
2022-03-25 11:09:52 -0600

    culled a bunch and got particles working

--- Snake Drop/Assets/Scripts/Blocks/BlockAnimators/Sprite Animators/DirectionalSpriteController.cs
+++ Snake Drop/Assets/Scripts/Blocks/BlockAnimators/Sprite Animators/DirectionalSpriteController.cs
@@ -11,9 +11,6 @@ public class DirectionalSpriteController : BlockSpriteAnimator
     public Sprite LeftSprite;
     public Sprite RightSprite;
     public bool HighlightTail;
-    public Material snakeMaterial;//Added by Alyssa
-    public Material notSnakeMaterial;
-    private Material currentMaterial;
     public Color color;
     public int sortingOrder;
 
@@ -21,15 +18,6 @@ public class DirectionalSpriteController : BlockSpriteAnimator
     {
         Block block = blockAnimation.block;
         Directions.Direction dir = Directions.Direction.DOWN;
-        if (block.blockType.isPartOfSnake == true)//Alyssa added this if
-        {
-            currentMaterial = snakeMaterial;
-        }
-        else
-        {
-            currentMaterial = notSnakeMaterial;
-        }
-
 
         if (block && block.Owner && block.Owner.playerController && block.Owner.RoundInProgress) dir = block.Owner.playerController.MostRecentDirectionMoved;
 
@@ -42,16 +30,16 @@ public class DirectionalSpriteController : BlockSpriteAnimator
         switch (dir) 
         {
             case Directions.Direction.UP:
-                SetSprite(block, UpSprite, color, sortingOrder, currentMaterial);//Alyssa added materials to these
+                SetSprite(block, UpSprite, color, sortingOrder);
                 break;
             case Directions.Direction.DOWN:
-                SetSprite(block, DownSprite, color, sortingOrder, currentMaterial);
+                SetSprite(block, DownSprite, color, sortingOrder);
                 break;
             case Directions.Direction.LEFT:
-                SetSprite(block, LeftSprite, color, sortingOrder, currentMaterial);
+                SetSprite(block, LeftSprite, color, sortingOrder);
                 break;
             case Directions.Direction.RIGHT:
-                SetSprite(block, RightSprite, color, sortingOrder, currentMaterial);
+                SetSprite(block, RightSprite, color, sortingOrder);
                 break;
         }
     }
