94f3b78804efa380ee81b8980a584e69c68b0790
Fano <n.j.dobek@gmail.com>
2022-04-13 22:32:42 -0600

    gradient and another attempt at canvas helper

--- Snake Drop/Assets/CanvasHelper.cs
+++ Snake Drop/Assets/CanvasHelper.cs
@@ -5,29 +5,33 @@ using UnityEngine.UI;
 
 public class CanvasHelper : MonoBehaviour
 {
-    private CanvasScaler canvasScaler;
-    private float bottomUnits, topUnits;
     void Start()
     {
-        canvasScaler = GetComponent<CanvasScaler>();
         ApplyVerticalSafeArea();
     }
 
 
     public void ApplyVerticalSafeArea()
     {
-        var bottomPixels = Screen.safeArea.y;
-        var topPixel = Screen.currentResolution.height - (Screen.safeArea.y + Screen.safeArea.height);
-
-        var bottomRatio = bottomPixels / Screen.currentResolution.height;
-        var topRatio = topPixel / Screen.currentResolution.height;
-
-        var referenceResolution = canvasScaler.referenceResolution;
-        bottomUnits = referenceResolution.y * bottomRatio;
-        topUnits = referenceResolution.y * topRatio;
 
         var rectTransform = GetComponent<RectTransform>();
-        rectTransform.offsetMin = new Vector2(rectTransform.offsetMin.x, bottomUnits);
-        rectTransform.offsetMax = new Vector2(rectTransform.offsetMax.x, -topUnits);
+
+        rectTransform.offsetMin = new Vector2(rectTransform.offsetMin.x, Screen.safeArea.y);
+        rectTransform.offsetMax = new Vector2(rectTransform.offsetMax.x, Screen.safeArea.height);
+
+
+        //var bottomPixels = Screen.safeArea.y;
+        //var topPixel = Screen.currentResolution.height - (Screen.safeArea.y + Screen.safeArea.height);
+
+        //var bottomRatio = bottomPixels / Screen.currentResolution.height;
+        //var topRatio = topPixel / Screen.currentResolution.height;
+
+        //var referenceResolution = canvasScaler.referenceResolution;
+        //bottomUnits = referenceResolution.y * bottomRatio;
+        //topUnits = referenceResolution.y * topRatio;
+
+        //var rectTransform = GetComponent<RectTransform>();
+        //rectTransform.offsetMin = new Vector2(rectTransform.offsetMin.x, bottomUnits);
+        //rectTransform.offsetMax = new Vector2(rectTransform.offsetMax.x, -topUnits);
     }
 }
--- Snake Drop/Assets/Purchase_Manager.cs
+++ Snake Drop/Assets/Purchase_Manager.cs
@@ -1,12 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Purchasing;
 
-public class Purchase_Manager : MonoBehaviour
+public class Purchase_Manager : MonoBehaviour/*, IStoreListener*/
 {
+    //private IStoreController controller;
+    //private IExtensionProvider extensions;
+
     public void OnPurchase()
     {
         CloudOnce.CloudVariables.UnlimitedUndos = true;
         CloudOnce.Cloud.Storage.Save();
     }
+
+    ///// <summary>
+    ///// Called when Unity IAP is ready to make purchases.
+    ///// </summary>
+    //public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    //{
+    //    this.controller = controller;
+    //    this.extensions = extensions;
+    //    Debug.Log("IT WORRRKJSSDEIJF");
+    //}
+
+    ///// <summary>
+    ///// Called when Unity IAP encounters an unrecoverable initialization error.
+    /////
+    ///// Note that this will not be called if Internet is unavailable; Unity IAP
+    ///// will attempt initialization until it becomes available.
+    ///// </summary>
+    //public void OnInitializeFailed(InitializationFailureReason error)
+    //{
+    //}
+
+    ///// <summary>
+    ///// Called when a purchase completes.
+    /////
+    ///// May be called at any time after OnInitialized().
+    ///// </summary>
+    //public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+    //{
+    //    return PurchaseProcessingResult.Complete;
+    //}
+
+    ///// <summary>
+    ///// Called when a purchase fails.
+    ///// </summary>
+    //public void OnPurchaseFailed(Product i, PurchaseFailureReason p)
+    //{
+    //}
 }
