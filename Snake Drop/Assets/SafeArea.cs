39fa2ee037a2ee50d4802c122a1ec4af7eafcdb8
Fano <n.j.dobek@gmail.com>
2022-04-15 14:54:01 -0600

    ui properly scales to safe area

---
+++ Snake Drop/Assets/SafeArea.cs
@@ -0,0 +1,28 @@
+//source: https://www.youtube.com/watch?v=VprqsEsFb5w
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SafeArea : MonoBehaviour
+{
+    private RectTransform t;
+    private Rect safeArea;
+    private Vector2 minAnchor;
+    private Vector2 maxAnchor;
+
+    private void Awake()
+    {
+        t = GetComponent<RectTransform>();
+        safeArea = Screen.safeArea;
+        minAnchor = safeArea.position;
+        maxAnchor = minAnchor + safeArea.size;
+
+        minAnchor.x /= Screen.width;
+        minAnchor.y /= Screen.height;
+        maxAnchor.x /= Screen.width;
+        maxAnchor.y /= Screen.height;
+
+        t.anchorMin = minAnchor;
+        t.anchorMax = maxAnchor;
+    }
+}
