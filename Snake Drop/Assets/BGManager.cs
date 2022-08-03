747990b37badb0c8379ae7a3360426df769c31a6
Fano <n.j.dobek@gmail.com>
2022-08-03 14:14:16 -0600

    BGmanager now fades

--- Snake Drop/Assets/BGManager.cs
+++ Snake Drop/Assets/BGManager.cs
@@ -22,21 +22,23 @@ public class BGManager : MonoBehaviour
     private void Awake()
     {
         if (!inst) inst = this;
-        foreach (BGSettings bg in backgrounds) bg.BGObject.SetActive(false);
-        ActiveBG.BGObject.SetActive(true);
+        SnapTo(ActiveBG);
     }
 
     private int activeBGIndex = 0;
     public int ActiveBGIndex
     {
         get { return activeBGIndex; }
-        set { ActiveBG.BGObject.SetActive(false);
+        set {
+            SnapTo(ActiveBG);
+            StartCoroutine(Fade(ActiveBG, false));
             activeBGIndex = value;
-            ActiveBG.BGObject.SetActive(true);
+            StartCoroutine(Fade(ActiveBG, true));
         }
     }
 
     public BGSettings[] backgrounds;
+    public float fadeTime;
     public BGSettings ActiveBG
     {
         get { return backgrounds[activeBGIndex % backgrounds.Length]; }
@@ -64,8 +66,66 @@ public class BGManager : MonoBehaviour
     private void Update()
     {
         PlayerManager p = GameManager.instance.playerManagers[0];
-        ActiveBGIndex = p.Powerup.numOfPowerupsObtained;
+        int i = p.Powerup.numOfPowerupsUsed;
+        if (i != ActiveBGIndex) ActiveBGIndex = i;
         p.playGrid.UpdateAllSprites();
         p.previewGrid.UpdateAllSprites();
+    }
+
+    private IEnumerator Fade(BGSettings BG, bool inOut)
+    {
+        SpriteRenderer[] BGRenderers = BG.BGObject.GetComponentsInChildren<SpriteRenderer>();
+        float startTime = Time.time;
+        float percentageComplete;
+
+        while (true)
+        {
+            percentageComplete = (Time.time - startTime) / fadeTime;
+
+            foreach(SpriteRenderer renderer in BGRenderers)
+            {
+                Color c = renderer.color;
+                if (inOut)
+                {
+                    c.a = Mathf.Clamp(percentageComplete, 0, 1);
+                }
+                else
+                {
+                    c.a = Mathf.Clamp(1 - percentageComplete, 0, 1);
+                }
+                renderer.color = c;
+
+            }
+
+            if(percentageComplete >= 1) 
+            { 
+                break; 
+            }
+
+            yield return null;
+        }
+    }
+
+    private void SnapTo(BGSettings BG)
+    {
+        foreach(BGSettings background in backgrounds)
+        {
+            SpriteRenderer[] BGRenderers = background.BGObject.GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer renderer in BGRenderers)
+            {
+                Color c = renderer.color;
+                if (background.BGObject == BG.BGObject)
+                {
+                    c.a = 1;
+                }
+                else
+                {
+                    c.a = 0;
+                }
+                renderer.color = c;
+
+            }
+
+        }
     }
 }
